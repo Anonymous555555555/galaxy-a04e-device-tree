0e1c4fc2712bcebdd44ace01460a5cd548d8db05
Showen Otto <showenotto@proton.me>
2025-04-12 19:00:41 +0200

    1.0 Initial commit

--- device/samsung/a04e/Android.mk
+++ device/samsung/a04e/Android.mk
@@ -9,37 +9,3 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),a04e)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
-
-# Modules
-## Google Play Store
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := GooglePlayStore
-#LOCAL_SRC_FILES := prebuilt/Phonesky.apk
-#LOCAL_MODULE_CLASS := APPS
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_SUFFIX := .apk
-#LOCAL_CERTIFICATE := PRESIGNED
-#LOCAL_PRIVILEGED_MODULE := true
-#include $(BUILD_PREBUILT)
-
-## Google Play Services
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := GmsCore
-#LOCAL_SRC_FILES := prebuilt/GmsCore.apk
-#LOCAL_MODULE_CLASS := APPS
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_SUFFIX := .apk
-#LOCAL_CERTIFICATE := PRESIGNED
-#LOCAL_PRIVILEGED_MODULE := true
-#include $(BUILD_PREBUILT)
-
-## Google Services Framework
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := GoogleServicesFramework
-#LOCAL_SRC_FILES := prebuilt/GoogleServicesFramework.apk
-#LOCAL_MODULE_CLASS := APPS
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_SUFFIX := .apk
-#LOCAL_CERTIFICATE := PRESIGNED
-#LOCAL_PRIVILEGED_MODULE := true
-#include $(BUILD_PREBUILT)
